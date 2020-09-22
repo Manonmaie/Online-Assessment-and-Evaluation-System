565ddc5a3c9211c1a7adba60ceb1e373a64e4106
NSaiAkshara <Sai.Akshara@iiitb.org>
2020-09-22 19:58:04 +0530

    Adding Instructions Page

--- Assessment/assessmentBackEnd/src/main/java/com/iiitb/assessmentBackEnd/questionPaper/QuestionPaperRepository.java
+++
@@ -1,7 +0,0 @@
-package com.iiitb.assessmentBackEnd.questionPaper;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface QuestionPaperRepository extends CrudRepository<AsQuestionPaper, Integer> {
-	
-}
