b9c732891e53cbc16662cdf3c401db4cfbd91116
Manonmaie <manonmaiepasumarthy@gmail.com>
2020-09-25 03:09:58 +0530

    Fix DB bug - Exam Admin

--- ExamAdmin/oaes-exam-admin-db.sql
+++ ExamAdmin/oaes-exam-admin-db.sql
@@ -88,16 +88,18 @@ CREATE TABLE IF NOT EXISTS ea_batch(
   batch_end_time datetime NOT NULL,
   qp_status ENUM('PENDING','RECEIVED','ERROR_SENDING') DEFAULT 'PENDING',
   center_id int(10) unsigned,
+  course_master_id int(10) unsigned,
   PRIMARY KEY (batch_id)
 );
 
 ALTER TABLE ea_batch
-  ADD CONSTRAINT `fk_ea_batch_center_id` FOREIGN KEY (center_id) REFERENCES ea_center(center_id) ON DELETE SET NULL;
+  ADD CONSTRAINT `fk_ea_batch_center_id` FOREIGN KEY (center_id) REFERENCES ea_center(center_id) ON DELETE SET NULL,
+  ADD CONSTRAINT `fk_ea_batch_course_master_id` FOREIGN KEY (course_master_id) REFERENCES ea_course_master(course_master_id) ON DELETE SET NULL;
 
 -- --------------------------------------------------------
 -- Data Entry for table ea_batch
 -- --------------------------------------------------------
-INSERT INTO ea_batch VALUES(0,"MRNG","2020-09-23 10:00:00","2020-09-23 13:00:00","RECEIVED",1);
+INSERT INTO ea_batch VALUES(0,"MRNG","2020-09-23 10:00:00","2020-09-23 13:00:00","RECEIVED",1,1);
 
 -- --------------------------------------------------------
 -- Table structure for table `ea_examinee_batch`
