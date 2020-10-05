0c6fd7d08f14f4fa5af9171c2098d49c14f98129
Manonmaie <manonmaiepasumarthy@gmail.com>
2020-10-06 03:41:37 +0530

    Examinees Bulk Add - Exam Admin

--- ExamAdmin/examAdminUI/src/app/examinee-add-bulk/examinee-add-bulk.component.ts
+++ ExamAdmin/examAdminUI/src/app/examinee-add-bulk/examinee-add-bulk.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Examinee } from '../shared/examinee';
+import { ExamineeService } from '../services/examinee.service';
+import { Params, ActivatedRoute, Router } from '@angular/router';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-examinee-add-bulk',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./examinee-add-bulk.component.scss']
 })
 export class ExamineeAddBulkComponent implements OnInit {
-
-  constructor() { }
-
+  examineeList:Examinee[] = [];
+  constructor(private examineeService: ExamineeService, public route: Router) { }
   ngOnInit(): void {
   }
 
+  onFileChange(ev){
+    let workBook = null;
+    let jsonData = null;
+    const reader = new FileReader();
+    const file = ev.target.files[0];
+    reader.onload = (event) => {
+      const data = reader.result;
+      workBook = XLSX.read(data, { type: 'binary' });
+      jsonData = workBook.SheetNames.reduce((initial, name) => {
+        const sheet = workBook.Sheets[name];
+        initial["1"] = XLSX.utils.sheet_to_json(sheet);
+        return initial["1"];
+      }, {});
+      this.examineeList = jsonData;
+    }
+    reader.readAsBinaryString(file);
+  }
+
+  addExamineeBulk(examineeList: Examinee[]): void{
+    this.examineeService.addExamineeBulk(examineeList).subscribe((examineeList) => this.examineeList=examineeList);
+  }
+
+  onSubmit(){
+    this.addExamineeBulk(this.examineeList);
+    this.route.navigate(['/examinees']);
+  }
 }
