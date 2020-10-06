0ec92b96dc26081a8588cf0629895695bfd70777
soumyakariveda <karivedasoumya@gmail.com>
2020-10-06 12:15:20 +0530

    itemTF api added

--- Authoring/Authoring_frontend/src/app/app-routing.module.ts
+++ Authoring/Authoring_frontend/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ItemFormComponent} from './item-form/item-form.component'
-import { QuestionPaperComponent } from './question-paper/question-paper.component';
+import { itemForm } from './question-paper/question-paper.component';
 import { QuestionManagementComponent } from './question-management/question-management.component'
 
 const routes: Routes = [
   {   path: 'itemadd',   component: ItemFormComponent   },
-  {   path: 'questionPaperCreate', component:QuestionPaperComponent},
+  {   path: 'questionPaperCreate', component:itemForm},
   {   path:'questionManagement', component:QuestionManagementComponent}
 ];
 
