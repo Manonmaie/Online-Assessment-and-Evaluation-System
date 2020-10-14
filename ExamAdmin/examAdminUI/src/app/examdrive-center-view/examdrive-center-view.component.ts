2e04990be92d08c610dec8bab91de0bf520f902f
Manonmaie <manonmaiepasumarthy@gmail.com>
2020-10-14 21:37:15 +0530

    Examdrive Center View - Exam Admin

---
+++ ExamAdmin/examAdminUI/src/app/examdrive-center-view/examdrive-center-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-examdrive-center-view',
+  templateUrl: './examdrive-center-view.component.html',
+  styleUrls: ['./examdrive-center-view.component.scss']
+})
+export class ExamdriveCenterViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
