0acb2593c7629031b97e46c377baffa41e1e6588
Manonmaie <manonmaiepasumarthy@gmail.com>
2020-10-14 21:17:28 +0530

    Examdrive View - Exam Admin

--- ExamAdmin/examAdminUI/src/app/examdrive-view/examdrive-view.component.ts
+++ ExamAdmin/examAdminUI/src/app/examdrive-view/examdrive-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ExamdriveService } from '../services/examdrive.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Examdrive } from '../shared/examdrive';
+import { Center } from '../shared/center';
 import {Observable, of} from 'rxjs';
 
 @Component({
@@ -11,14 +12,39 @@ import {Observable, of} from 'rxjs';
 })
 export class ExamdriveViewComponent implements OnInit {
   examdrive: Examdrive;
+  centers: Center[] = new Array();
   constructor(private examdriveService:ExamdriveService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     const examdriveId = this.route.snapshot.params['id'];
     this.getExamdrive(examdriveId);
+    setTimeout(() => {
+      this.getCenters();
+    },500);
   }
 
   getExamdrive(id: number): void{
     this.examdriveService.getExamdrive(id).subscribe((examdrive) => this.examdrive=examdrive);
+  }
+
+  getCenters(): void{
+    for( var batch of this.examdrive.batchList){
+      this.centers.push(batch.center);
+    }
+    this.centers = this.centers.filter(
+      (thing, i, arr) => arr.findIndex(t => t.centerId === thing.centerId) === i
+    );
+  }
+
+  parseObject(obj)
+  {
+    for(var key in obj)
+    {
+        console.log("key: " + key + ", value: " + obj[key])
+        if(obj[key] instanceof Object)
+        {
+          this.parseObject(obj[key]);
+        }
+    }
   }
 }
