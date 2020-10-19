76a48b8216443c18831a055ec03c64a39caf1016
Manonmaie <manonmaiepasumarthy@gmail.com>
2020-10-19 20:58:22 +0530

    import Services in AppModule

--- ExamAdmin/examAdminUI/src/app/app.module.ts
+++ ExamAdmin/examAdminUI/src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ExamdriveComponent } from './examdrive/examdrive.component';
-import { HeaderComponent } from './header/header.component';
-import { CenterComponent } from './center/center.component';
-import { ExamineeComponent } from './examinee/examinee.component';
-
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 // import { HttpModule } from '@angular/http';
 import {baseURL} from './shared/baseurl';
 import { FormsModule } from '@angular/forms';
 
-import {ExamdriveService} from './services/examdrive.service';
+import { BatchService } from './services/batch.service';
+import { CenterService } from './services/center.service';
+import { CourseService } from './services/course.service';
+import { ExamdriveService } from './services/examdrive.service';
+import { ExamineeBatchService } from './services/examinee-batch.service';
+import { ExamineeService } from './services/examinee.service';
+import { ExcelServicesService } from './services/excel-services.service';
+import { InterceptorService } from './services/interceptor.service';
 
-import {AppRoutingModule} from './app-routing/app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { AppComponent } from './app.component';
+import { ExamdriveComponent } from './examdrive/examdrive.component';
+import { HeaderComponent } from './header/header.component';
+import { CenterComponent } from './center/center.component';
+import { ExamineeComponent } from './examinee/examinee.component';
 import { CourseComponent } from './course/course.component';
 import { CenterViewComponent } from './center-view/center-view.component';
 import { CourseViewComponent } from './course-view/course-view.component';
@@ -23,7 +28,6 @@ import { CenterAddComponent } from './center-add/center-add.component';
 import { CourseAddComponent } from './course-add/course-add.component';
 import { CenterUpdateComponent } from './center-update/center-update.component';
 import { CourseUpdateComponent } from './course-update/course-update.component';
-import { InterceptorService } from './services/interceptor.service';
 import { ExamineeViewComponent } from './examinee-view/examinee-view.component';
 import { ExamineeUpdateComponent } from './examinee-update/examinee-update.component';
 import { ExamineeAddComponent } from './examinee-add/examinee-add.component';
@@ -67,7 +71,14 @@ import { ExamdriveCenterViewComponent } from './examdrive-center-view/examdrive-
     FormsModule
   ],
   providers: [
+    BatchService,
+    CenterService,
+    CourseService,
     ExamdriveService,
+    ExamineeBatchService,
+    ExamineeService,
+    ExcelServicesService,
+    InterceptorService,
     {provide: 'BaseURL', useValue: baseURL},
     {provide: HTTP_INTERCEPTORS, useClass: InterceptorService,multi: true}
   ],
