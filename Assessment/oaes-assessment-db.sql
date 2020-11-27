43eadfa1984112ee5f5982d9f6c58d7d6feb0708
NSaiAkshara <Sai.Akshara@iiitb.org>
2020-11-27 14:27:54 +0530

    Shuffle according to examineeId

--- Assessment/oaes-assessment-db.sql
+++ Assessment/oaes-assessment-db.sql
@@ -208,7 +208,7 @@ ALTER TABLE as_batch
 -- --------------------------------------------------------
 -- Data Entry for table `as_batch`
 -- --------------------------------------------------------
-INSERT INTO as_batch VALUES(0,"Mrng1","2020-11-24 07:00:00","2020-11-24 23:00:00","SENT",1,1,NULL);
+INSERT INTO as_batch VALUES(0,"Mrng1","2020-11-27 07:00:00","2020-11-27 23:00:00","SENT",1,1,NULL);
 INSERT INTO as_batch VALUES(0,"AfterNoon1","2020-09-28 14:00:00","2020-09-28 23:59:59","RECEIVED",1,2,NULL);
 INSERT INTO as_batch VALUES(0,"Mrng2","2020-01-01 09:00:00","2020-01-01 12:00:00","SENT",1,3,NULL);
 -- INSERT INTO as_batch VALUES(0,"Mrng3","2020-01-01 09:00:00","2020-01-01 12:00:00","SENT",1,3,NULL);
