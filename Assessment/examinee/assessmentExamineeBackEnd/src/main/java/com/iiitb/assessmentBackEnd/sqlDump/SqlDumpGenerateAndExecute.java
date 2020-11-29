c0f51afd4ab3bf503c72313fcfdec31d5b0851ef
NSaiAkshara <Sai.Akshara@iiitb.org>
2020-11-29 19:27:54 +0530

    Dump File - Generation and Execution

---
+++ Assessment/examinee/assessmentExamineeBackEnd/src/main/java/com/iiitb/assessmentBackEnd/sqlDump/SqlDumpGenerateAndExecute.java
@@ -0,0 +1,42 @@
+package com.iiitb.assessmentBackEnd.sqlDump;
+
+import java.io.*;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SqlDumpGenerateAndExecute {
+
+	public void createSqlDumpFile() throws IOException {
+		System.out.println("creating dump");
+	      Runtime rt = Runtime.getRuntime();
+	      Process p = rt.exec("/usr/local/Cellar/mysql/8.0.12/bin/mysqldump -u root oaes_assessment_db as_user as_role_master");
+//	      mysqldump -u [user name] -p[password] [database name] > [dump file]
+	      InputStream is=p.getInputStream();
+	      FileOutputStream fos=new FileOutputStream("dump.sql");
+	      int ch;
+	      while((ch=is.read())!=-1) {
+	             fos.write(ch);
+	      }
+	      fos.close();
+	      is.close();
+	      System.out.println("----SQL backup file generated: mydb_abackup.sql----");
+	    }
+
+	    public boolean runSqlDumpFile() throws IOException, InterruptedException {
+	      Runtime rt = Runtime.getRuntime();
+	      String dbUsername="root", dbPassword="akshara", dbName="dummy", sourceFile="dump.sql";
+//	      Process pr = rt.exec("mysql -p -h ServerName dummy < mydb_abackup.sql");
+	      String[] command = new String[]{
+	                "/usr/local/Cellar/mysql/8.0.12/bin/mysql",
+	                "-u" + dbUsername,
+	                "-p" + dbPassword,
+	                "-e",
+	                " source " + sourceFile,
+	                dbName
+	        };
+	        Process runtimeProcess = Runtime.getRuntime().exec(command);
+	        int processComplete = runtimeProcess.waitFor();
+	        return processComplete == 0;
+	    }
+}
