3310f00f3794421abb766f9782a68682a086bf55
Manonmaie <manonmaiepasumarthy@gmail.com>
2020-12-01 14:50:24 +0530

    Send EPack - Frontend - Display Key - ExamAdmin

--- ExamAdmin/examAdminUI/src/app/services/package-management.service.ts
+++ ExamAdmin/examAdminUI/src/app/services/package-management.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Observable, of} from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { baseURL } from '../shared/baseurl';
+import { Status } from '../shared/status';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,9 @@ export class PackageManagementService {
 
   importMPack(key: string): Observable<any>{
     return this.http.get<any>(baseURL+'importFromMpack/'+key);
+  }
+
+  exportEPack(centerId: number): Observable<Status>{
+    return this.http.get<Status>(baseURL+'createEpack/'+centerId);
   }
 }
